751d2c2df21e50f6ddf3c9ba1db9195323cfcc4e
Miikaar <mkkaarto@gmail.com>
2015-06-09 14:39:58 +0300

    Finish login/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   	@user = User.new(user_params)  # Not the final impl
   	if @user.save
    		# Success
+      log_in @user
    		flash[:success] = "Welcome to the Sample App!"
    		redirect_to @user
   	else
